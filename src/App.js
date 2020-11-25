f62b853a23a414660844664b9fa3fc238f06bb11
RyanPark0128 <vazywon@hotmail.com>
2020-11-24 17:31:54 -0800

    feat:add axios request on the data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cards from './components/Cards/Cards'
 import Chart from './components/Chart/Chart'
 import CountryPicker from './components/CountryPicker/CountryPicker'
 import './App.css'
-
+import { fetchData } from './api/index';
 
 const App = () => {
+    const [data, setData] = useState({})
+    const dataHandle = async () => {
+        const fetchedData = await fetchData();
+        setData(fetchedData);
+    }
+
+    useEffect(() => {
+        dataHandle()
+    },[]);
 
     return (
         <div className="container">
-            <Cards />
+            <Cards data={data}/>
             <CountryPicker />
             <Chart />
         </div>
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import "./Cards.css"
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
-const Cards = () => {
+const Cards = ( { data: { confirmed, recovered, deaths, lastUpdate } }) => {
+
+    if (!confirmed) {
+        return 'Loading';
+    }
     return (
-        <h1>Cards</h1>
+        <div className="container">
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">{confirmed.value}</Typography>
+                        <Typography color="textSecondary">Real Date</Typography>
+                        <Typography variant="body2">Number of active cases of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">Real Data</Typography>
+                        <Typography color="textSecondary">Real Date</Typography>
+                        <Typography variant="body2">Number of Recover cases of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Death</Typography>
+                        <Typography variant="h5">Real Data</Typography>
+                        <Typography color="textSecondary">Real Date</Typography>
+                        <Typography variant="body2">Number of death cases of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
